a3216bb9b9f9211c1ee282c71df19a6a85e06ab8
dy <978548481@qq.com>
2016-07-30 22:17:37 +0800

    加车搞定

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,7 @@ package com.company;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
+import java.util.*;
 
 
 public class Main {
@@ -15,15 +12,22 @@ public class Main {
         // write your code here
 
         String animTime = "2016-07-30 21:00:00";
-        Init.init();
-        ArrayList<HashMap<String, String>> userList = Init.getUserList();
-        String[] GoodsList={
-                "df2854829060345587,df160727p2854829,1",
-                "df2854829131469803,df160727p2854829,1"
-        };
+        System.out.println("例子如下:");
+        System.out.println(animTime);
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()){
+            animTime=scanner.nextLine().trim();
+            Init.init();
+            ArrayList<HashMap<String, String>> userList = Init.getUserList();
+            String[] GoodsList={
+                    "df2854829060345587,df160727p2854829,1",
+                    "df2854829131469803,df160727p2854829,1"
+            };
 //        HashMap<String, String> stringStringHashMap = userList.get(0);
-        for (int i = 0; i < userList.size(); i++) {
-            new AddCartThread(animTime,GoodsList, userList.get(i)).run();
+            for (int i = 0; i < userList.size(); i++) {
+                new AddCartThread(animTime,GoodsList, userList.get(i)).run();
+            }
         }
+
     }
 }
